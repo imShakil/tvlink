43def4997a03eb5f1136014f342fd69713027e47
Mobarak Hosen Shakil <20867846+imShakil@users.noreply.github.com>
2024-06-16 07:46:44 +0600

    Update bdixtv247.sh

--- bdixtv247.sh
+++ bdixtv247.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-wget -O bdixtv247.m3u8 https://bdixtv247.techpriyo.com/m3u/play32.php?e=.m3u
+wget -O bdixtv247.m3u8 https://bdixtv247.xyz/m3u/index.php?e=.m3u
 
-sed -i '1,3d' bdixtv247.m3u8
+#sed -i '1,3d' bdixtv247.m3u8
