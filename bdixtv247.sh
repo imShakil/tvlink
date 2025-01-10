72bbaf82c7a8b8eb05094c2a5e92738d08b25b53
Mobarak Hosen Shakil <mh.ice.iu@gmail.com>
2025-01-10 17:29:41 +0600

    Update bdixtv247.sh

--- bdixtv247.sh
+++ bdixtv247.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-wget -O https://raw.githubusercontent.com/aniketda/iptvtest/refs/heads/main/iptvtest
+wget https://raw.githubusercontent.com/aniketda/iptvtest/refs/heads/main/iptvtest -O iptv.m3u8
 
 #sed -i '1,3d' bdixtv247.m3u8
